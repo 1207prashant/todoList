6a26adf0070c1d34187c438fb42f3f4d60930f9e
1207prashant <prasantdwivedi7@gmail.com>
2022-09-07 14:13:37 +0530

    todo

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero/hero";
+export  const HEROES:Hero[]=[
+    {id:10 ,name:'Yogesh Sir' },
+    {id:11,name:'Amit Kumar'},
+    {id:12,name:'Prashant Dwivedi'},
+    {id:13,name:'Pankaj'},
+    {id:14,name:'Chetan'},
+    {id:15,name:'Aman'},
+    {id:16,name:'Abhinav'},
+    {id:17,name:'Vinod'},
+    {id:18,name:'Shelendra'},
+    {id:19,name:'Rahul'},
+    {id:20,name:'vimlesh'},
+
+]
